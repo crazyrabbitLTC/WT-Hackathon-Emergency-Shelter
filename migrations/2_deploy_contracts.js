164a0571e74ad059b46fb4ab271bf6bba5054f5f
Skylar Weaver <sweaver@deloitte.com>
2018-10-25 10:59:09 +0200

    Auto deployment of contracts using truffle

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,15 @@
 var EmergencyShelterIndex = artifacts.require("EmergencyShelterIndex");
+var WTIndex = artifacts.require("WTIndex");
 
-module.exports = function(deployer) {
-  // deployer.deploy(WTIndex);
-  deployer.deploy(EmergencyShelterIndex);
+module.exports = async function(deployer) {
+  
+  // Deploy WTIndex
+  await deployer.deploy(WTIndex);
+  console.log('>>>>> WTIndex Deployed <<<<<');
+  const wTIndexInstance = await WTIndex.deployed();
+  const wTIndexAddress = wTIndexInstance.address;
+  
+  // Deploy EmergencyShelterIndex
+  await deployer.deploy(EmergencyShelterIndex, wTIndexAddress);
+  console.log('>>>>> EmergencyShelterIndex Deployed <<<<<');
 };
